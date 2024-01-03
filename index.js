b3ef9818c237f9fd0a899ee6a611301613f919ca
Deeplai-Gobare <deepaligobar07@gmail.com>
2024-01-04 04:11:44 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
@@ -7,7 +7,7 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.get("/", (req, res) => {
 //   res.render("index.ejs");
